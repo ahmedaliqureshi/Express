721504359d7abf6c13952d5afa88100fe64039f7
Ahmed <ahmedaliqureshi59@gmail.com>
2025-11-26 09:48:54 +0000

    my second api

---
+++ server2/app.js
@@ -0,0 +1,12 @@
+//why:- we are importing express because we need to use 
+// methods like the port,put and get 
+
+//where:- we are doing this at the start of the program 
+//so we can use it through out the program 
+
+// what:- we are goig to use import key word and pass the 
+// package name in strin \
+
+import express from "express"; 
+
+// 
